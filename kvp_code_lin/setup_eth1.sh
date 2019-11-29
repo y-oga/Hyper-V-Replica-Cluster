b4af3004cd9cef01a985aae86b81f320b9a8c471
ogata <y-ogata@hg.jp.nec.com>
2019-11-29 22:32:59 +0900

    Clean up

--- kvp_code_lin/setup_eth1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-nmcli c m eth1 ipv4.address $1 ipv4.method manual
-nmcli c down eth1
-nmcli c up eth1
--- kvp_code_win/VM_setup.bat
+++
@@ -1,2 +0,0 @@
-call .\config\VM_config.bat
-PowerShell -File .\script\setup_routerVM.ps1
