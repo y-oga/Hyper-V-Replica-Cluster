221be4dc67d63c81034c6116bcd0c231cb835136
ogata <y-ogata@hg.jp.nec.com>
2020-01-31 16:43:10 +0900

    Bug Fix

--- WithRouterVM/ForWS2019/AutoScripts/tool5/setup_ecx_Primary/setup_ecx.ps1
+++ WithRouterVM/ForWS2019/AutoScripts/tool5/setup_ecx_Primary/setup_ecx.ps1
@@ -9,8 +9,8 @@ $srIp = $env:SECONDARY_IP_ADDRESS_TO_PUB
 $targetVM = $env:APP_VM_NAME
 $adminPass = $env:ADMIN_PASS
 
-$pHostname = $pHostname.ToLower();
-$sHostname = $sHostname.ToLower();
+$pHostname = $pHostname.ToLower()
+$sHostname = $sHostname.ToLower()
 
 $path = ".\scripts\failover\replica_script\cluster_config.bat"
 $file_contents = $(Get-Content $path) -creplace "INPUT_PRIMARY_HOSTNAME",$pHostname
--- WithoutRouterVM/ForWS2019/AutoScripts/tool5/setup_ecx_Primary/setup_ecx.ps1
+++ WithoutRouterVM/ForWS2019/AutoScripts/tool5/setup_ecx_Primary/setup_ecx.ps1
@@ -7,8 +7,8 @@ $sIp = $env:SECONDARY_IP_ADDRESS
 $targetVM = $env:APP_VM_NAME
 $adminPass = $env:ADMIN_PASS
 
-$pHostname = $pHostname.ToLower();
-$sHostname = $sHostname.ToLower();
+$pHostname = $pHostname.ToLower()
+$sHostname = $sHostname.ToLower()
 
 $path = ".\scripts\failover\replica_script\cluster_config.bat"
 $file_contents = $(Get-Content $path) -creplace "INPUT_PRIMARY_HOSTNAME",$pHostname
